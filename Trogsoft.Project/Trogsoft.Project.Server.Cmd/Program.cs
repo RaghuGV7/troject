cab5c4fc23f5bba99de260006b4d9b316d34729c
Neil Bullock <neil@trogsoft.com>
2018-01-12 15:39:14 +0000

    Initial project structure.

---
+++ Trogsoft.Project/Trogsoft.Project.Server.Cmd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trogsoft.Project.Server.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
